bdb43e2389ddf21a321897446c040fb4751a2182
Houssem Saïah <houcemesaiah@gmail.com>
2024-01-09 01:03:21 -0500

    Users controller and routing

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,14 @@
 import express from 'express';
+import users from './users';
 
 import authentication from './authentication';
+
 
 const router = express.Router();
 
 export default (): express.Router => {
 authentication(router);
+users(router);
 
     return router;
 };
